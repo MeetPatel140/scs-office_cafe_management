81ff26b1565a6fa8a921aa62be95b5d47a4f8bbf
MeetPatel140 <meetsolanki140@gmail.com>
2024-03-13 11:50:30 +0530

    [13 Mar] [v1.1] Code Safe

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :doorkeeper_authorize!, only: [:index, :show, :create]
+  before_action :doorkeeper_authorize!, except: [:login, :logout]
+  skip_before_action :verify_authenticity_token
+
   def index
     @users = User.all
     render json: { users: @users, message: "You have Succesfully Tested and Setup the Rails Public API" }, status: :ok
@@ -18,33 +22,56 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: params[:user][:email])
+    if user&.valid_password?(params[:user][:password])
+      client_app = Doorkeeper::Application.find_by(uid: params[:client_id])
+      access_token = Doorkeeper::AccessToken.create!(
+        resource_owner_id: user.id,
+        application_id: client_app.id,
+        refresh_token: generate_refresh_token, # Implement generate_refresh_token method
+        expires_in: Doorkeeper.configuration.access_token_expires_in.to_i,
+        scopes: ''
+      )
+      render json: { user: user, access_token: access_token.token, message: 'Login successful' }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  def logout
+    if params[:access_token].present?
+      current_token = Doorkeeper::AccessToken.find_by(token: params[:access_token])
+      if current_token
+        destroyed = current_token.destroy
+        if destroyed
+          render json: { message: 'Logout successful' }, status: :ok
+        else
+          render json: { error: 'Failed to destroy access token' }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Invalid access token' }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Access token not provided' }, status: :unauthorized
+    end
+  end
+
+
+  def logout1
+    current_token = doorkeeper_token
+    current_token.revoke if current_token.present?
+    render json: { message: 'Logout successful' }, status: :ok
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :phone)
   end
 
-  def render_not_found
-    render json: { error: "User not found" }, status: :not_found
+  def generate_refresh_token
+    SecureRandom.hex(32)
   end
 
-  def render_unprocessable_entity
-    render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-  end
-
-  def render_invalid
-    render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-  end
-
-  def render_created
-    render json: @user
-  end
-
-  def render_updated
-    render json: @user
-  end
-
-  def render_deleted
-    render json: @user
-  end
 end
